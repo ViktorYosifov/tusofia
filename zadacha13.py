45ff5184046a4349a019c133ec62a3fb605f1b9c
Viktor <vktrysv@gmail.com>
2023-12-12 15:44:07 +0200

    kursova rabota algoritmi

---
+++ zadacha13.py
@@ -0,0 +1,94 @@
+#възел в свързания списък със стойност и указател към следващия възел
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+class DynamicCircularLinkedList:
+
+    def __init__(self):
+        #указател към първия възел
+        self.head = None
+        #указател към последния възел
+        self.tail = None
+        self.size = 0
+
+    def append(self, value):
+        new_node = Node(value)
+        if not self.head:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            self.tail = new_node
+
+        self.size += 1
+
+        if self.size >= 2 ** (self.size.bit_length() - 1):
+            self.double_size()
+
+    def double_size(self):
+        current = self.head
+        for _ in range(self.size - 1):
+            current = current.next
+
+        self.tail.next = current.next
+        self.tail = current
+
+    def delete(self, value):
+        if not self.head:
+            print("List is empty")
+            return
+
+        current = self.head
+        prev = None
+        found = False
+
+        while current and not found:
+            if current.data == value:
+                found = True
+            else:
+                prev = current
+                current = current.next
+
+        if found:
+            if prev:
+                prev.next = current.next
+                if current == self.tail:
+                    self.tail = prev
+            else:
+                self.head = current.next
+                if current == self.tail:
+                    self.tail = None
+
+
+            self.size -= 1
+            print(f"{value} is deleted")
+        else:
+            print(f"{value} is not in the list")
+
+    def display(self):
+        current = self.head
+        if not current:
+            print("List is empty")
+        else:
+            while current:
+                print(current.data, end="->")
+                current = current.next
+                if current == self.head:
+                    break
+            print()
+
+
+if __name__ == "__main__":
+    list = DynamicCircularLinkedList()
+    list.append(1)
+    list.append(2)
+    list.append(3)
+    list.display()
+    list.append(4)
+    list.display()
+    list.delete(3)
+    list.display()
+    list.append(5)
+    list.display()
---
+++ zadacha16.py
@@ -0,0 +1,67 @@
+#възела в двоичното дърво
+class TreeNode:
+    def __init__(self, value):
+        self.value = value
+        #указател къв ляво поддърво
+        self.left = None
+        #указател къв дясно поддърво
+        self.right = None
+
+class BinaryTree:
+    def __init__(self, root):
+        self.root = TreeNode(root)
+
+    def print_values(self, start, traversal_type, condition):
+        if traversal_type == "inorder":
+            return self._inorder(start, [], condition)
+        elif traversal_type == "preorder":
+            return self._preorder(start, [], condition)
+        elif traversal_type == "postorder":
+            return self._postorder(start, [], condition)
+        else:
+            print("Invalid traversal type")
+            return None
+
+    #left-root-right
+    def _inorder(self, start, result, condition):
+        if start:
+            result = self._inorder(start.left, result, condition)
+            if condition(start.value):
+                result.append(start.value)
+            result = self._inorder(start.right, result, condition)
+        return result
+
+    #root-left-right
+    def _preorder(self, start, result, condition):
+        if start:
+            if condition(start.value):
+                result.append(start.value)
+            result = self._preorder(start.left, result, condition)
+            result = self._preorder(start.right, result, condition)
+        return result
+
+    #left-right-root
+    def _postorder(self, start, result, condition):
+        if start:
+            result = self._postorder(start.left, result, condition)
+            result = self._postorder(start.right, result, condition)
+            if condition(start.value):
+                result.append(start.value)
+        return result
+
+
+if __name__ == "__main__":
+    tree = BinaryTree(1)
+    tree.root.left = TreeNode(-2)
+    tree.root.right = TreeNode(3)
+    tree.root.left.left = TreeNode(-4)
+    tree.root.right.left = TreeNode(-5)
+    tree.root.right.right = TreeNode(6)
+
+    positive_values = tree.print_values(tree.root, "inorder", lambda x: x > 0)
+    print("Positive values(Inorder): ", positive_values)
+
+    negative_values = tree.print_values(tree.root, "inorder", lambda x: x < 0)
+    print("Negative values(Inorder): ", negative_values)
+
+
